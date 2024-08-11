8f5485c5ab2c60223007e745599be451cb54f0dc
Vishal490404 <desaivishal4904@gmail.com>
2024-08-11 15:33:00 +0530

    Commit by V

--- -/bind_it/move.sh
+++ -/bind_it/move.sh
@@ -13,7 +13,16 @@ encode_file(){
 passwords=("DEMO" "Q4OS" "MX" "Void" "Bodhi" "antiX" "Solus" "NixOS" "Alpine")
 
 # Password check
-read -p "Please enter the password for next level (or type 'stop' to exit the game): " password
+read -p "Please enter the password for the next level (or type 'stop' to exit the game): " password
+
+decode_file
+
+# Get the current level
+curr_level=$(sed -n '2p' /etc/app/.txt)
+curr_level=$(( curr_level + 1 ))
+
+# Get the last password from the file
+last_password=$(tail -n 1 /etc/app/.txt)
 
 if [[ $password == "stop" ]]; then
     echo "You may now exit by either pressing Ctrl + D or typing 'exit' (without quotes)."
@@ -21,21 +30,24 @@ if [[ $password == "stop" ]]; then
     exit 93
 fi
 
-decode_file
 
-curr_level=$(sed -n '2p' /etc/app/.txt)
-curr_level=$(( curr_level + 1 ))
-
-encode_file
+if [[ $last_password == "${passwords[$((curr_level - 1))]}" ]]; then
+    echo "You have already entered the correct password for this level."
+    echo "You may now exit by either pressing Ctrl + D or typing 'exit' (without quotes)."
+    encode_file
+    exit 0
+fi
 
 if [[ $curr_level -ge 0 && $curr_level -le ${#passwords[@]} && $password == "${passwords[$((curr_level))]}" ]]; then
-    # sed -i "2s/.*/$curr_level/" /etc/app/.txt
-    # encode_file
-    echo "Password is correct. Level updated to $(( curr_level + 1 ))."
+    # Update the level and append the password to the file
+    sed -i "2s/.*/$curr_level/" /etc/app/.txt
+    echo "$password" >> /etc/app/.txt
+    encode_file
+    echo "Password is correct. Level updated to $((curr_level+1))."
     echo "You may now exit by either pressing Ctrl + D or typing 'exit' (without quotes)."
-    exit 26
+    exit 0
 else
-    # encode_file
     echo "Incorrect flag! Please try again."
+    encode_file
     exit 0
 fi
